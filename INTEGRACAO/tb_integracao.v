76b9adcb0598d59a9bef520ce619be39bffbb269
DavidOSilva <davidoliveirasilvaa@gmail.com>
2023-11-25 04:50:28 -0300

    Atualização da integração e novo modulo filtro!

--- INTEGRACAO/tb_integracao.v
+++ INTEGRACAO/tb_integracao.v
@@ -2,12 +2,13 @@ module tb_integracao;
 
 	reg clk, rstn, pop, push, load;
 	reg [4:0] opcode;
-	reg [7:0] din;
+	reg [7:0] imm, data;
 	wire empty, full, carryout;
 	wire [7:0] s_ula, tmp1, tmp2, outpilha;
 	
-	integracao tb(.clk(clk), .rstn(rstn), .pop(pop), .push(push), .opcode(opcode), .din(din), .load(load),
-					  .empty(empty), .full(full), .carryout(carryout), .s_ula(s_ula), .tmp1(tmp1), .tmp2(tmp2), .outpilha(outpilha));
+	integracao tb(.imm(imm), .data(data), .rstn(rstn), .clk(clk), .load(load), .opcode(opcode), .empty(empty),
+					  .full(full), .carryout(carryout), .s_ula(s_ula), .pop(pop), .push(push), .tmp1(tmp1), .tmp2(tmp2),
+					  .outpilha(outpilha));
 					  
 	always #5 clk = ~clk;
 	
@@ -17,61 +18,80 @@ module tb_integracao;
 	rstn = 1'b0;
 	pop = 1'b0;
 	push = 1'b0;
-	din = 8'd0;
+	imm = 8'd0;
+	data = 8'd0;
 	#30 rstn = 1'b1;
 	end
 	endtask
 	
-	task ler();
+	task lerPilha();
 	begin
 	pop = 1'b1;
 	#10 pop = 1'b0;
 	end
 	endtask
-   
-	task escrever(input [7:0]din_tb);
-	begin
-	push = 1'b1;
-	din = din_tb;
-	#10 push = 1'b0;
-	end
-	endtask
 	
-	task carregar();
+	task carregarRegistradores();
 	begin
+	lerPilha();
+	#10;
 	load = 1'b1;
 	#10 load = 1'b0;
 	end
 	endtask
 	
-	task calcular();
+	task add();
 	begin
-	ler();
-	carregar();
+	opcode = 5'b00100;
+	carregarRegistradores();
 	#10;
-	
-	ler();
-	carregar();
+	carregarRegistradores();
 	#10;
+	push = 1'b1;
+	#10 push = 1'b0;
 	end
 	endtask
 	
-	initial begin
+	task push_d(input [7:0]data_tb);
+	begin
+	opcode = 5'b00000;
+	data = data_tb;
+	push = 1'b1;
+	#10 push = 1'b0;
+	end
+	endtask
 	
-		opcode = 5'b00100;
-		#10;
+	task push_i(input [7:0]imm_tb);
+	begin
+	opcode = 5'b00001;
+	imm = imm_tb;
+	push = 1'b1;
+	#10 push = 1'b0;
+	end
+	endtask
+	
+	task push_t();
+	begin
+	opcode = 5'b00010;
+	push = 1'b1;
+	#10 push = 1'b0;
+	end
+	endtask
+	
+	
+	initial begin
 	
 		reset();
 		#10;
 		
 		//Enchendo pilha
-		escrever(8'd12);
+		push_i(8'd12);
+		#10;
+		push_i(8'd15);
 		#10;
 		
-		escrever(8'd15);
-		#10;
-		
-		calcular();
+		//Operando
+		add();
 		#10;
 		
 		$finish;
