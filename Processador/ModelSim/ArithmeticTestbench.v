b716ee2983f645608e02a51d7656641fbb91601c
Paloma <paloma.arize@ifood.com.br>
2023-12-12 23:02:56 -0300

    refactor tests classes

--- Processador/ModelSim/ArithmeticTestbench.v
+++ Processador/ModelSim/ArithmeticTestbench.v
@@ -37,193 +37,169 @@ begin
     
 end
 
+parameter  add = 5'b00_100,
+        mul = 5'b00_110,
+        div = 5'b00_111,
+        sub = 5'b00_101;
+
 always @ (DUV.b2v_inst4.state) 
 begin
-    //--------------------------------CENARIOS NRS POSITIVOS ----------------------------------------------------
-    if(DUV.b2v_inst4.ir == 5'b00_100)
-    begin
-        
-        if(DUV.b2v_inst4.state == 5'hA && DUV.b2v_inst2.in1 == 8'h2)
-        begin
-            $display("--------------Inicia teste ADD -------------------");
-            $display("Soma os 2 ultimos valores da pilha e os retira da pilha");
-            display_operandos;
-            idx = DUV.b2v_inst3.index;
-            
-            if(DUV.b2v_inst3.stack[idx] === 5'h9)
-            begin
-                display_tos;
-                display_esperado;
-                $display("--------------Finaliza teste ADD -------------------");
-                cenarios_sucesso = cenarios_sucesso + 1;
-            end
-            else    display_inesperado;
-        end
-    end	
-    else if(DUV.b2v_inst4.ir == 5'b00_110)
-    begin
-        
-        if(DUV.b2v_inst4.state == 5'hA && DUV.b2v_inst2.in1 == 8'h3)
-        begin
-            $display("--------------Inicia teste MUL -------------------");
-            $display("Multiplica os 2 ultimos valores da pilha e os retira da pilha");
-            display_operandos;
-            idx = DUV.b2v_inst3.index;
-            
-            if(DUV.b2v_inst3.stack[idx] === 5'h1b)
-            begin
-                display_tos;
-                display_esperado;
-                $display("--------------Finaliza teste MUL -------------------");
-                cenarios_sucesso = cenarios_sucesso + 1;
-            end
-            else    display_inesperado;
-        end
-    end 
-    else if(DUV.b2v_inst4.ir == 5'b00_111)
-    begin
-        
-        if(DUV.b2v_inst4.state == 5'hA && DUV.b2v_inst2.in1 == 8'h36)
-        begin
-            $display("--------------Inicia teste DIV -------------------");
-            $display("Divide o primeiro da pilha pelo segundo e os retira da pilha");
-            display_operandos;
-            idx = DUV.b2v_inst3.index;
-            
-            if(DUV.b2v_inst3.stack[idx] === 5'h2)
-            begin
-                display_tos;
-                display_esperado;
-                $display("--------------Finaliza teste DIV -------------------");
-                cenarios_sucesso = cenarios_sucesso + 1;
-            end
-            else 
-            display_inesperado;
-        end
-    end	
-    else if(DUV.b2v_inst4.ir == 5'b00_101)
-    begin
-        
-        if(DUV.b2v_inst4.state == 5'hA && DUV.b2v_inst2.in1 == 8'h8)
-        begin
-            $display("--------------Inicia teste SUB -------------------");
-            $display("Subtrai o primeiro da pilha pelo segundo e os retira da pilha");
-            display_operandos;
-            idx = DUV.b2v_inst3.index;
-            
-            if(DUV.b2v_inst3.stack[idx] === 5'h6)
-            begin
-                display_tos;
-                display_esperado;
-                $display("--------------Finaliza teste SUB -------------------");
-                cenarios_sucesso = cenarios_sucesso + 1;
-            end
-            else    display_inesperado;
-        end
-    end	
-
-    //--------------------------------CENARIOS NRS NEGATIVOS ------------------------------------------
-    if(DUV.b2v_inst4.ir == 5'b00_101)
-    begin
-        
-        if(DUV.b2v_inst4.state == 5'hA && DUV.b2v_inst2.in1 == 8'h4)
-        begin
-            $display("--------------Inicia teste SUB com resultado negativo -------------");
-            $display("Subtrai o primeiro da pilha pelo segundo e os retira da pilha");
-            display_operandos;
-            idx = DUV.b2v_inst3.index;
-            
-            if($signed(DUV.b2v_inst3.stack[idx]) === -2)
-            begin
-                display_tos_negativo;
-                display_esperado;
-                $display("--------------Finaliza teste SUB com resultado negativo--------------");
-                cenarios_sucesso = cenarios_sucesso + 1;
-            end
-            else display_inesperado;
-        end
-    end	
-    else if(DUV.b2v_inst4.ir == 5'b00_111)
-    begin
-        
-        if(DUV.b2v_inst4.state == 5'hA && DUV.b2v_inst2.in1 == 8'h2)
-        begin
-            $display("--------------Inicia teste DIV com resultado negativo -------------");
-            $display("Divide o primeiro da pilha pelo segundo e os retira da pilha");
-            display_operandos;
-            idx = DUV.b2v_inst3.index;
-            
-            if($signed(DUV.b2v_inst3.stack[idx]) === -1)
-            begin
-                display_tos_negativo;
-                display_esperado;
-                $display("--------------Finaliza teste SUB com resultado negativo--------------");
-                cenarios_sucesso = cenarios_sucesso + 1;
-            end
-            else display_inesperado;
-        end
-    end
-    else if(DUV.b2v_inst4.ir == 5'b00_110)
-    begin
-        
-        if(DUV.b2v_inst4.state == 5'hA  && DUV.b2v_inst2.in1 == 8'h12)
-        begin
-            $display("--------------Inicia teste MUL com resultado negativo -------------------");
-            $display("Multiplica os 2 ultimos valores da pilha e os retira da pilha");
-            display_operandos;
-            idx = DUV.b2v_inst3.index;
-            
-            if($signed(DUV.b2v_inst3.stack[idx]) === -18)
-            begin
-                display_tos_negativo;
-                display_esperado;
-                $display("--------------Finaliza teste MUL com resultado negativo ----------------");
-                cenarios_sucesso = cenarios_sucesso + 1;
-            end
-            else display_inesperado;
-        end
-    end	
-    else if(DUV.b2v_inst4.ir == 5'b00_110)
-    begin
-        
-        if(DUV.b2v_inst4.state == 5'hA  && DUV.b2v_inst2.in1 == 8'h12)
-        begin
-            $display("--------------Inicia teste MUL com resultado negativo -------------------");
-            $display("Multiplica os 2 ultimos valores da pilha e os retira da pilha");
-            display_operandos;
-            idx = DUV.b2v_inst3.index;
-            
-            if($signed(DUV.b2v_inst3.stack[idx]) === -18)
-            begin
-                display_tos_negativo;
-                display_esperado;
-                $display("--------------Finaliza teste MUL com resultado negativo ----------------");
-                cenarios_sucesso = cenarios_sucesso + 1;
-            end
-            else display_inesperado;
-        end
-    end	
-    else if(DUV.b2v_inst4.ir == 5'b00_100)
-    begin
-        
-        if(DUV.b2v_inst4.state == 5'hA  && DUV.b2v_inst2.in1 == 8'h11)
-        begin
-            $display("--------------Inicia teste ADD com resultado negativo -------------------");
-            $display("Soma os 2 ultimos valores da pilha e os retira da pilha");
-            display_operandos;
-            idx = DUV.b2v_inst3.index;
-            
-            if($signed(DUV.b2v_inst3.stack[idx]) === -1)
-            begin
-                display_tos_negativo;
-                display_esperado;
-                $display("--------------Finaliza teste MUL com resultado negativo ----------------");
-                cenarios_sucesso = cenarios_sucesso + 1;
-            end
-            else    display_inesperado;
-        end
-    end	
+    case (DUV.b2v_inst4.ir)
+        add: testa_add;
+        mul: testa_mul;
+        div: testa_div;
+        sub: testa_sub; 
+    endcase
 end
+
+task testa_add;
+begin
+    if(DUV.b2v_inst4.state == 5'hA && DUV.b2v_inst2.in1 == 8'h2)
+    begin
+        $display("--------------Inicia teste ADD -------------------");
+        $display("Soma os 2 ultimos valores da pilha e os retira da pilha");
+        display_operandos;
+        idx = DUV.b2v_inst3.index;
+        
+        if(DUV.b2v_inst3.stack[idx] === 5'h9)
+        begin
+            display_tos;
+            display_esperado;
+            $display("--------------Finaliza teste ADD -------------------");
+            cenarios_sucesso = cenarios_sucesso + 1;
+        end
+        else    display_inesperado;
+    end
+    else if(DUV.b2v_inst4.state == 5'hA  && DUV.b2v_inst2.in1 == 8'h11)
+    begin
+        $display("--------------Inicia teste ADD com resultado negativo -------------------");
+        $display("Soma os 2 ultimos valores da pilha e os retira da pilha");
+        display_operandos;
+        idx = DUV.b2v_inst3.index;
+        
+        if($signed(DUV.b2v_inst3.stack[idx]) === -1)
+        begin
+            display_tos_negativo;
+            display_esperado;
+            $display("--------------Finaliza teste MUL com resultado negativo ----------------");
+            cenarios_sucesso = cenarios_sucesso + 1;
+        end
+        else    display_inesperado;
+    end
+end
+endtask
+
+task testa_mul;
+begin
+    if(DUV.b2v_inst4.state == 5'hA && DUV.b2v_inst2.in1 == 8'h3)
+    begin
+        $display("--------------Inicia teste MUL -------------------");
+        $display("Multiplica os 2 ultimos valores da pilha e os retira da pilha");
+        display_operandos;
+        idx = DUV.b2v_inst3.index;
+        
+        if(DUV.b2v_inst3.stack[idx] === 5'h1b)
+        begin
+            display_tos;
+            display_esperado;
+            $display("--------------Finaliza teste MUL -------------------");
+            cenarios_sucesso = cenarios_sucesso + 1;
+        end
+        else    display_inesperado;
+    end
+    else if(DUV.b2v_inst4.state == 5'hA  && DUV.b2v_inst2.in1 == 8'h12)
+    begin
+        $display("--------------Inicia teste MUL com resultado negativo -------------------");
+        $display("Multiplica os 2 ultimos valores da pilha e os retira da pilha");
+        display_operandos;
+        idx = DUV.b2v_inst3.index;
+        
+        if($signed(DUV.b2v_inst3.stack[idx]) === -18)
+        begin
+            display_tos_negativo;
+            display_esperado;
+            $display("--------------Finaliza teste MUL com resultado negativo ----------------");
+            cenarios_sucesso = cenarios_sucesso + 1;
+        end
+        else display_inesperado;
+    end
+end
+endtask
+
+task testa_div;
+begin
+    if(DUV.b2v_inst4.state == 5'hA && DUV.b2v_inst2.in1 == 8'h36)
+    begin
+        $display("--------------Inicia teste DIV -------------------");
+        $display("Divide o primeiro da pilha pelo segundo e os retira da pilha");
+        display_operandos;
+        idx = DUV.b2v_inst3.index;
+        
+        if(DUV.b2v_inst3.stack[idx] === 5'h2)
+        begin
+            display_tos;
+            display_esperado;
+            $display("--------------Finaliza teste DIV -------------------");
+            cenarios_sucesso = cenarios_sucesso + 1;
+        end
+        else 
+        display_inesperado;
+    end
+    else if(DUV.b2v_inst4.state == 5'hA && DUV.b2v_inst2.in1 == 8'h2)
+    begin
+        $display("--------------Inicia teste DIV com resultado negativo -------------");
+        $display("Divide o primeiro da pilha pelo segundo e os retira da pilha");
+        display_operandos;
+        idx = DUV.b2v_inst3.index;
+        
+        if($signed(DUV.b2v_inst3.stack[idx]) === -1)
+        begin
+            display_tos_negativo;
+            display_esperado;
+            $display("--------------Finaliza teste DIV com resultado negativo--------------");
+            cenarios_sucesso = cenarios_sucesso + 1;
+        end
+        else display_inesperado;
+    end
+end
+endtask
+
+task testa_sub;
+begin
+    if(DUV.b2v_inst4.state == 5'hA && DUV.b2v_inst2.in1 == 8'h8)
+    begin
+        $display("--------------Inicia teste SUB -------------------");
+        $display("Subtrai o primeiro da pilha pelo segundo e os retira da pilha");
+        display_operandos;
+        idx = DUV.b2v_inst3.index;
+        
+        if(DUV.b2v_inst3.stack[idx] === 5'h6)
+        begin
+            display_tos;
+            display_esperado;
+            $display("--------------Finaliza teste SUB -------------------");
+            cenarios_sucesso = cenarios_sucesso + 1;
+        end
+        else    display_inesperado;
+    end
+    else if(DUV.b2v_inst4.state == 5'hA && DUV.b2v_inst2.in1 == 8'h4)
+    begin
+        $display("--------------Inicia teste SUB com resultado negativo -------------");
+        $display("Subtrai o primeiro da pilha pelo segundo e os retira da pilha");
+        display_operandos;
+        idx = DUV.b2v_inst3.index;
+        
+        if($signed(DUV.b2v_inst3.stack[idx]) === -2)
+        begin
+            display_tos_negativo;
+            display_esperado;
+            $display("--------------Finaliza teste SUB com resultado negativo--------------");
+            cenarios_sucesso = cenarios_sucesso + 1;
+        end
+        else display_inesperado;
+    end
+end
+endtask
 
 task display_tos;
 begin
